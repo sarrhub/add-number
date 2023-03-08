ceafdc2c3990bf7c9f016ad977ac49168d76e5a8
sarang <sarangmotekar@gmail.com>
2023-03-08 05:50:48 -0800

    initial commit

---
+++ addition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the value "<<endl;
+    cout<<"a=";
+    cin>>a;
+    cout<<"b=";
+    cin>>b;
+    cout<<a<<"+"<<b<<"="<<a+b;
+
+
+
+}
